774e640a9022ccb8f762cdce9c80f2acb589f3fa
Adrian Madiedo <madiedoadrian445@gmail.com>
2025-10-28 22:17:00 -0400

    Make description optional for Service model

--- src/models/Service.js
+++ src/models/Service.js
@@ -4,12 +4,11 @@ import mongoose from "mongoose";
 const serviceSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    description: { type: String, required: true },
+    description: { type: String, required: false }, // 👈 made optional
     price: { type: Number, required: true },
     category: { type: String },
     location: { type: String },
     photos: [{ type: String }],
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   },
   { timestamps: true }
 );
