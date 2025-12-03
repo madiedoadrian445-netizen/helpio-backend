1297e9ddaa1af9d9da0f8ce64fffa3234b5345c8
Adrian Madiedo <madiedoadrian445@gmail.com>
2025-12-02 22:49:04 -0500

    FORCE RESET: remove routes folder

--- src/routes/service.routes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/service.routes.js
-import express from "express";
-import { createService, getAllServices } from "../controllers/service.controller.js";
-const router = express.Router();
-
-router.get("/", getAllServices);
-router.post("/", createService);
-
-export default router;
