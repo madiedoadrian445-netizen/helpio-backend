9452dbb862b13f746684ced2d5de315ccc1abde2
Adrian Madiedo <madiedoadrian445@gmail.com>
2026-02-14 17:37:25 -0500

    Fix feed route imports (correct auth + controller paths)

--- src/routes/feedRoutes.js
+++ src/routes/feedRoutes.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { getFeed } from "../controllers/listingsFeedController.js";
-import authMiddleware from "../middleware/authMiddleware.js";
-
+import auth from "../middleware/auth.js";
 const router = express.Router();
 
 // GET /api/feed
