ed22d304b1297f245aeb88feedd51f7a3feee3b6
Adrian Madiedo <madiedoadrian445@gmail.com>
2026-03-04 12:49:11 -0500

    Fix auth middleware import for review routes

--- src/routes/review.routes.js
+++ src/routes/review.routes.js
@@ -2,17 +2,12 @@
 
 import express from "express";
 import * as ctl from "../controllers/review.controller.js";
-import auth from "../middleware/auth.js";
+import * as authMiddleware from "../middleware/auth.js";
 
 const router = express.Router();
 
-// POST review
-router.post("/", auth(true), ctl.addReview);
-
-// GET reviews
+router.post("/", authMiddleware.auth(true), ctl.addReview);
 router.get("/:serviceId", ctl.listForService);
-
-// DELETE review
-router.delete("/:id", auth(true), ctl.removeReview);
+router.delete("/:id", authMiddleware.auth(true), ctl.removeReview);
 
 export default router;
